c286ec9847ed7b5e1c11feeacdbb131c639fc4e3
Joy <gajjarjoy4@gmail.com>
2021-06-03 14:24:38 +0530

    Language Translator

---
+++ main.py
@@ -0,0 +1,5 @@
+from google_trans_new import google_translator
+translator = google_translator()
+text = input("Enter a text: ")
+translate = translator.translate(text, lang_tgt='gu')
+print(translate)
